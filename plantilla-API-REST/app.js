f8f6b9d7622969110ad3cb50e59cc024945b2e77
Umuberuto <43342458+Umuberuto@users.noreply.github.com>
2018-09-17 21:50:23 +0200

    Todas las operaciones con sus 2 opciones

--- plantilla-API-REST/app.js
+++ plantilla-API-REST/app.js
@@ -40,11 +40,49 @@ app.get('/calculadora/resta/:num1/:num2', (req, res, next) => {
         metodo: 'params'
     })
 })
+app.get('/calculadora/resta/', (req, res, next) => {
+    var resultado = Number(req.query.num1) - Number(req.query.num2);
+    res.status(200).json({
+        ok: true,
+        resultado: resultado,
+        metodo: 'query'
+    })
+})
 app.get('/calculadora/multi/:num1/:num2', (req, res, next) => {
     var resultado = Number(req.params.num1) * Number(req.params.num2);
     res.status(200).json({
         ok: true,
         resultado: resultado,
         metodo: 'params'
+    })
+})
+app.get('/calculadora/multi/', (req, res, next) => {
+    var resultado = Number(req.query.num1) * Number(req.query.num2);
+    res.status(200).json({
+        ok: true,
+        resultado: resultado,
+        metodo: 'query'
+    })
+})
+app.get('/calculadora/divi/:num1/:num2', (req, res, next) => {
+    if(Number(req.params.num2)!=0)
+        var resultado = Number(req.params.num1) / Number(req.params.num2);
+    else
+        var resultado = "No se puede dividir entre 0.";
+    res.status(200).json({
+        ok: true,
+        resultado: resultado,
+        metodo: 'params'
+    })
+})
+app.get('/calculadora/divi/', (req, res, next) => {
+    if(Number(req.query.num2)!=0)
+        var resultado = Number(req.query.num1) * Number(req.query.num2);
+    else
+        var resultado = "No se puede dividir entre 0.";
+    res.status(200).json({
+        ok: true,
+        resultado: resultado,
+        metodo: 'query'
     })
 })
