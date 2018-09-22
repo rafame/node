68c82f9440db2fcf5b72f8026b4feba5382ff139
rafame <rmj250@gmail.com>
2018-09-22 20:59:06 +0200

    primos

--- plantilla-API-REST/app.js
+++ plantilla-API-REST/app.js
@@ -1,9 +1,9 @@
 // Requires
-var express = require('express');
+let express = require('express');
 
-//Iniciar variables
-var app = express();
-var port = 3000;
+//Iniciar letiables
+let app = express();
+let port = 3000;
 
 //Escuchar peticiones
 app.listen(port, () => {
@@ -17,7 +17,7 @@ function verde() {
 
 //Rutas
 app.get('/calculadora/suma/:num1/:num2', (req, res, next) => {
-    var resultado = Number(req.params.num1) + Number(req.params.num2);
+    let resultado = Number(req.params.num1) + Number(req.params.num2);
     res.status(200).json({
         ok: true,
         resultado: resultado,
@@ -25,7 +25,7 @@ app.get('/calculadora/suma/:num1/:num2', (req, res, next) => {
     })
 })
 app.get('/calculadora/suma/', (req, res, next) => {
-    var resultado = Number(req.query.num1) + Number(req.query.num2);
+    let resultado = Number(req.query.num1) + Number(req.query.num2);
     res.status(200).json({
         ok: true,
         resultado: resultado,
@@ -33,7 +33,7 @@ app.get('/calculadora/suma/', (req, res, next) => {
     })
 })
 app.get('/calculadora/resta/:num1/:num2', (req, res, next) => {
-    var resultado = Number(req.params.num1) - Number(req.params.num2);
+    let resultado = Number(req.params.num1) - Number(req.params.num2);
     res.status(200).json({
         ok: true,
         resultado: resultado,
@@ -41,7 +41,7 @@ app.get('/calculadora/resta/:num1/:num2', (req, res, next) => {
     })
 })
 app.get('/calculadora/resta/', (req, res, next) => {
-    var resultado = Number(req.query.num1) - Number(req.query.num2);
+    let resultado = Number(req.query.num1) - Number(req.query.num2);
     res.status(200).json({
         ok: true,
         resultado: resultado,
@@ -49,7 +49,7 @@ app.get('/calculadora/resta/', (req, res, next) => {
     })
 })
 app.get('/calculadora/multi/:num1/:num2', (req, res, next) => {
-    var resultado = Number(req.params.num1) * Number(req.params.num2);
+    let resultado = Number(req.params.num1) * Number(req.params.num2);
     res.status(200).json({
         ok: true,
         resultado: resultado,
@@ -57,7 +57,7 @@ app.get('/calculadora/multi/:num1/:num2', (req, res, next) => {
     })
 })
 app.get('/calculadora/multi/', (req, res, next) => {
-    var resultado = Number(req.query.num1) * Number(req.query.num2);
+    let resultado = Number(req.query.num1) * Number(req.query.num2);
     res.status(200).json({
         ok: true,
         resultado: resultado,
@@ -65,10 +65,11 @@ app.get('/calculadora/multi/', (req, res, next) => {
     })
 })
 app.get('/calculadora/divi/:num1/:num2', (req, res, next) => {
+    let resultado=0;
     if(Number(req.params.num2)!=0)
-        var resultado = Number(req.params.num1) / Number(req.params.num2);
+        resultado = Number(req.params.num1) / Number(req.params.num2);
     else
-        var resultado = "No se puede dividir entre 0.";
+        resultado = "No se puede dividir entre 0.";
     res.status(200).json({
         ok: true,
         resultado: resultado,
@@ -76,10 +77,62 @@ app.get('/calculadora/divi/:num1/:num2', (req, res, next) => {
     })
 })
 app.get('/calculadora/divi/', (req, res, next) => {
+    let resultado=0;
     if(Number(req.query.num2)!=0)
-        var resultado = Number(req.query.num1) * Number(req.query.num2);
+        resultado = Number(req.query.num1) * Number(req.query.num2);
     else
-        var resultado = "No se puede dividir entre 0.";
+        resultado = "No se puede dividir entre 0.";
+    res.status(200).json({
+        ok: true,
+        resultado: resultado,
+        metodo: 'query'
+    })
+})
+app.get('/calculadora/descomponer/:num1', (req, res, next) => {
+    let num=Number(req.params.num1);
+    let nummax=num/2;
+    let divis=2;
+    let potencia=0;
+    let resultado={};
+    while (divis<nummax){
+        if (num%divis==0){
+            while (num%divis==0){
+                num/=divis;
+                potencia++;
+            }
+        }
+        if(potencia>0)
+            resultado[divis]=potencia;
+        potencia=0;
+        divis++;
+    }
+    
+    res.status(200).json({
+        ok: true,
+        resultado: resultado,
+        metodo: 'query'
+    })
+})
+app.get('/calculadora/primos/:num1', (req, res, next) => {
+    let nummax=Number(req.params.num1);
+    let num =2;
+    let fac=2;
+    let primo=true;
+    let resultado=[];
+    while (num<=nummax){
+            while ((fac<num)&&primo){
+                if(num%fac==0)
+                    primo=false;
+                fac++;
+            }
+        
+        if(primo)
+            resultado.push(num);
+        primo=true;
+        fac=2;
+        num++;
+    }
+    
     res.status(200).json({
         ok: true,
         resultado: resultado,
